50f45ab0cfa95d985dc64d8754cd1bac50ac3e29
riko910 <tarikulislamriko910@gmail.com>
2024-12-11 01:21:23 +0600

    master setup

---
+++ resources/views/backend/layouts/inc/script.blade.php
@@ -0,0 +1,16 @@
+<script src='{{asset('assets/backend')}}/assets/libs/choices.js/public/assets/scripts/choices.min.js'></script>
+<script src="{{asset('assets/backend')}}/assets/libs/%40popperjs/core/umd/popper.min.js"></script>
+<script src="{{asset('assets/backend')}}/assets/libs/tippy.js/tippy-bundle.umd.min.js"></script>
+<script src="{{asset('assets/backend')}}/assets/libs/simplebar/simplebar.min.js"></script>
+<script src="{{asset('assets/backend')}}/assets/libs/prismjs/prism.js"></script>
+<script src="{{asset('assets/backend')}}/assets/libs/lucide/umd/lucide.js"></script>
+<script src="{{asset('assets/backend')}}/assets/js/tailwick.bundle.js"></script>
+<!--apexchart js-->
+<script src="{{asset('assets/backend')}}/assets/libs/apexcharts/apexcharts.min.js"></script>
+<script src="https://apexcharts.com/samples/assets/stock-prices.js"></script>
+
+<!--dashboard analytics init js-->
+<script src="{{asset('assets/backend')}}/assets/js/pages/dashboards-analytics.init.js"></script>
+
+<!-- App js -->
+<script src="{{asset('assets/backend')}}/assets/js/app.js"></script>
---
+++ resources/views/backend/layouts/inc/style.blade.php
@@ -0,0 +1,10 @@
+<!-- App favicon -->
+<link rel="shortcut icon" href="{{asset('assets/backend')}}/assets/images/favicon.ico">
+<!-- Layout config Js -->
+<script src="{{asset('assets/backend')}}/assets/js/layout.js"></script>
+<!-- Icons CSS -->
+
+<!-- Tailwind CSS -->
+
+
+<link rel="stylesheet" href="{{asset('assets/backend')}}/assets/css/tailwind2.css">
