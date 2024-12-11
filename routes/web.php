978c774db5cee7ba355fb094e3983ec125e8764c
riko910 <tarikulislamriko910@gmail.com>
2024-12-11 23:58:41 +0600

    controlle

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('backend/pages/dashboard');
 });
+
+Route::get('/customer-list', [\App\Http\Controllers\CustomerListController::class, 'index'])->name('customer.list');
