3b3c49138878f22f0c8ea49fbe49c7db3ce6515d
RIKO910 <tarikulislamriko910@gmail.com>
2025-01-06 22:52:16 +0600

    web

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ Route::resource('employees', \App\Http\Controllers\CustomerListController::class
 Route::resource('/', \App\Http\Controllers\DashboardController::class);
 
 Route::post('/add-employee', [\App\Http\Controllers\CustomerListController::class, 'store'])->name('employee.store');
+
 //Route::post('/update-customer', [\App\Http\Controllers\CustomerListController::class, 'update'])->name('employee.update');
 Route::get('/employees/{id}/edit', [\App\Http\Controllers\CustomerListController::class, 'edit'])->name('employees.edit');
 
