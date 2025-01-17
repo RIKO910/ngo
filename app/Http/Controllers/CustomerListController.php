e1b79f8bfcfd4cd85d991cfb843b2ce4fdb01b94
RIKO910 <tarikulislamriko910@gmail.com>
2025-01-17 22:17:32 +0600

    controller

--- app/Http/Controllers/CustomerListController.php
+++ app/Http/Controllers/CustomerListController.php
@@ -30,6 +30,8 @@ class CustomerListController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+
+
     public function store(Request $request)
     {
         $request->validate([
